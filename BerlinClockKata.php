3a972a29eb700bc9b96ad4a2fcb27ce509c347c4
Stefan <stefan.mircovici@student.vinci.be>
2021-11-23 11:56:28 +0100

    add testMinutesSimples test

--- BerlinClockKata.php
+++ BerlinClockKata.php
@@ -5,15 +5,22 @@ class BerlinClockKata
     private $heures;
     private $minutes;
     private $secondes;
+    private $horloge;
 
     public function __construct($heures, $minutes, $secondes){
         $this->heures = $heures;
         $this->minutes = $minutes;
         $this->secondes = $secondes;
-        $this->createClock();
-    }
-    private function createClock(){
+        //$this->horloge = [['O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O','O','O','O','O','O','O','O'],['O','O','O','O']];
 
+    }
+    public function getMinutesSimples(){
+        $reste = $this->minutes%5;
+        $tabMinutesSimples = ['O','O','O','O'];
+        for($i=0;$i<$reste;$i++){
+            $tabMinutesSimples[$i]='Y';
+        }
+        return $tabMinutesSimples;
     }
 }
 
--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -1,9 +1,13 @@
 <?php
-
+require 'BerlinClockKata.php';
 
 use PHPUnit\Framework\TestCase;
 
 class BerlinClockKataTest extends TestCase
 {
-
+    public function testMinutesSimples(){
+        $berlinClock = new BerlinClockKata(12,32,00);
+        $actual = $berlinClock->getMinutesSimples();
+        $this->assertEquals("['Y','Y','O','O']",$actual);
+    }
 }
