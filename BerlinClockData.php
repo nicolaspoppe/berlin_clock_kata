331647984a83f973e9d9cbe6f71d70b07cecb34e
Nicolas Poppe <NicolasPoppe123@gmail.com>
2021-11-23 11:10:15 +0100

    ajout du constructeur et des attributs

--- BerlinClockData.php
+++ BerlinClockData.php
@@ -2,7 +2,19 @@
 
 class BerlinClockData
 {
+    private $heures;
+    private $minutes;
+    private $secondes;
 
+    public function __construct($heures, $minutes, $secondes){
+        $this->heures = $heures;
+        $this->minutes = $minutes;
+        $this->secondes = $secondes;
+        $this->createClock();
+    }
+    private function createClock(){
+
+    }
 }
 
 ?>
