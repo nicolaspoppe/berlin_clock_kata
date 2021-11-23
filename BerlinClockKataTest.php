60291aaf8e6f170e56724abb19f5a18e8754f61e
Stefan <stefan.mircovici@student.vinci.be>
2021-11-23 12:06:37 +0100

    finish testMinutesSimples

--- BerlinClockKataTest.php
+++ BerlinClockKataTest.php
@@ -8,6 +8,6 @@ class BerlinClockKataTest extends TestCase
     public function testMinutesSimples(){
         $berlinClock = new BerlinClockKata(12,32,00);
         $actual = $berlinClock->getMinutesSimples();
-        $this->assertEquals("['Y','Y','O','O']",$actual);
+        $this->assertEquals(['Y','Y','O','O'],$actual);
     }
 }
